15e74f79fe34da545592d456fa6b15e424796379
mauricio <mauriciogtec@gmail.com>
2024-06-08 12:52:25 -0400

    Add unified config for census and zcta Fixes #15

--- src/fetch_variables.py
+++ src/fetch_variables.py
@@ -1,23 +1,31 @@
-import requests
-import pandas as pd
-import numpy as np
+import logging
+
 import hydra
-import utils.validate as validate
+import numpy as np
+import pandas as pd
+import requests
+
+logger = logging.getLogger(__name__)
+
 
 def get_data(year, geo_type, dataset, variable_list, api_key):
 
-    #helper dictionary for URL creation
-    geography_dict = {'county': 'county:*', 'state': 'state:*', 'zcta': 'zip%20code%20tabulation%20area:*'}
+    # helper dictionary for URL creation
+    geography_dict = {
+        "county": "county:*",
+        "state": "state:*",
+        "zcta": "zip%20code%20tabulation%20area:*",
+    }
 
-    #extract the required geography for URL creation
+    # extract the required geography for URL creation
     geography = geography_dict[geo_type]
 
-    #structure for api endpoint
-    api_endpoint = f'https://api.census.gov/data/{year}/{dataset}'
+    # structure for api endpoint
+    api_endpoint = f"https://api.census.gov/data/{year}/{dataset}"
 
     # Construct the URL with the parameters
-    url = f'{api_endpoint}?get={variable_list}&for={geography}&key={api_key}'
-    #print(url)
+    url = f"{api_endpoint}?get={variable_list}&for={geography}&key={api_key}"
+    # print(url)
 
     # Make the API request
     response = requests.get(url)
@@ -25,9 +33,9 @@ def get_data(year, geo_type, dataset, variable_list, api_key):
     # Process the response
     if response.status_code == 200:
         status = 200
-        #save the response in json 
+        # save the response in json
         data = response.json()
-        #convert to dataframe 
+        # convert to dataframe
         df = pd.DataFrame(data[1:], columns=data[0])
         # The data variable now contains the requested data for all counties.
     else:
@@ -35,39 +43,42 @@ def get_data(year, geo_type, dataset, variable_list, api_key):
         df = None
 
     if status == 200:
-        if geo_type == 'county':
-            col_list = [geo_type, 'state'] + variable_list.split(',')
+        if geo_type == "county":
+            col_list = [geo_type, "state"] + variable_list.split(",")
             df = df[col_list]
-            df['county'] = df['county'] + df['state']
-            df.drop(columns=['state'], inplace=True)
-            return True, df.set_index('county')
-        if geo_type == 'state':
-            col_list = ['state'] + variable_list.split(',')
+            df["county"] = df["county"] + df["state"]
+            df.drop(columns=["state"], inplace=True)
+            return True, df.set_index("county")
+        if geo_type == "state":
+            col_list = ["state"] + variable_list.split(",")
             df = df[col_list]
-            return True, df.set_index('state')
-        if geo_type == 'zcta':
-            col_list = ['zip code tabulation area'] + variable_list.split(',')
+            return True, df.set_index("state")
+        if geo_type == "zcta":
+            col_list = ["zip code tabulation area"] + variable_list.split(",")
             df = df[col_list]
-            df.rename(columns={'zip code tabulation area':'zcta'},inplace=True)
-            return True, df.set_index('zcta')
-    
+            df.rename(columns={"zip code tabulation area": "zcta"}, inplace=True)
+            return True, df.set_index("zcta")
+
     else:
         return False, None
 
-def process_variable_dict(year, geo_type, dataset, variable_codes, variable_label, api_key):
+
+def process_variable_dict(
+    year, geo_type, dataset, variable_codes, variable_label, api_key
+):
     # == identify the variables to fetch ==
-    if 'den' in variable_codes: 
-        den_list = variable_codes['den']
-        num_list = variable_codes['num']
+    if "den" in variable_codes:
+        den_list = variable_codes["den"]
+        num_list = variable_codes["num"]
         if den_list is None and num_list is None:
             return None
         all_var_list = den_list + num_list
-    
+
     else:
-        num_list = variable_codes['num']
+        num_list = variable_codes["num"]
         all_var_list = num_list
 
-    variables = ','.join(all_var_list)
+    variables = ",".join(all_var_list)
 
     if variables is None:
         return None
@@ -77,8 +88,8 @@ def process_variable_dict(year, geo_type, dataset, variable_codes, variable_labe
 
     if status == True:
         # == transform to ratio ==
-        df = df.apply(pd.to_numeric, errors='coerce')
-        if 'den' in variable_codes:
+        df = df.apply(pd.to_numeric, errors="coerce")
+        if "den" in variable_codes:
             df[variable_label] = df[num_list].sum(axis=1) / df[den_list].sum(axis=1)
         else:
             df[variable_label] = df[num_list].sum(axis=1)
@@ -88,19 +99,21 @@ def process_variable_dict(year, geo_type, dataset, variable_codes, variable_labe
         # == clean df ==
         # Fill missing values with NaN
         df = df.fillna(np.nan)
-    
+
         numerical_columns = df.select_dtypes(include=[np.number]).columns
 
         # Replace negative values with NaN in numerical columns
         for column in numerical_columns:
-            df[column] = df[column].apply(lambda x: np.nan if pd.notna(x) and x < 0 else x)
+            df[column] = df[column].apply(
+                lambda x: np.nan if pd.notna(x) and x < 0 else x
+            )
 
         # Assign year
         # if dataset contains 'acs5', assign the midyear of the ACS5 5-year estimates
-        if 'acs5' in dataset:
-            df['year'] = year - 2
-        else: 
-            df['year'] = year
+        if "acs5" in dataset:
+            df["year"] = year - 2
+        else:
+            df["year"] = year
 
         # Reset index
         df.reset_index(inplace=True)
@@ -109,44 +122,40 @@ def process_variable_dict(year, geo_type, dataset, variable_codes, variable_labe
     else:
         return None
 
+
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(cfg):
-    validate.validate_census_variable(cfg)
-    variable = cfg.variable
-
     var_df_list = []
-    for survey in cfg.census.variables[variable]:
-        if cfg.census.variables[variable][survey].segments is None:
+
+    # get the codes for the geo_type/survey/variable
+    codes_segments = cfg.variable_codes[cfg.variable][cfg.survey]
+    year_cuts = [x.from_year for x in codes_segments]
+
+    for year in cfg.valid_years[cfg.geo_type][cfg.survey]:
+        # find first year such that year <= year_cut | cast np.int64 to int
+        index = int(np.searchsorted(year_cuts, year, side="right")) - 1
+        dataset = codes_segments[index].dataset
+
+        if dataset is None:  # cases when specific variable not available
             continue
-        for segment in cfg.census.variables[variable][survey].segments:
-            for year in segment.years:
-                dataset = segment['dataset']
-                variable_codes = segment['codes']
-                variable_label = variable
-                geo_type = cfg.census.geo_type
 
-                print(f"Generating for year: {year}, geo_type {geo_type}, dataset {dataset}, variable {variable_label}")
-                df = process_variable_dict(year, geo_type, dataset, variable_codes, variable_label, cfg.api_key)
-                if df is None:
-                    raise ValueError(f"Cannot generate for year: {year}, geo_type {geo_type}, dataset {dataset}, variable {variable_label}")
-                else:                            
-                    df['survey'] = survey
-                    # Set index
-                    if geo_type == 'county':
-                        df.set_index(['survey','year','county'], inplace=True)
-                    if geo_type == 'state':
-                        df.set_index(['survey','year','state'], inplace=True)
-                    if geo_type == 'zcta':
-                        df.set_index(['survey','year','zcta'], inplace=True)
-                    var_df_list.append(df)
+        # generate the variable dataframe
+        logger.info(
+            f"FETCHING year: {year}, geo_type: {cfg.geo_type}, survey: {cfg.survey}, variable: {cfg.variable}"
+        )
+        codes = codes_segments[index].codes
+        df = process_variable_dict(
+            year, cfg.geo_type, dataset, codes, cfg.variable, cfg.api_key
+        )
+        df["survey"] = cfg.survey
+        df.set_index(["survey", "year", cfg.geo_type], inplace=True)
+        var_df_list.append(df)
+
     var_df = pd.concat(var_df_list)
+    filename = f"{cfg.geo_type}__{cfg.survey}__{cfg.variable}.parquet"
+    var_df.to_parquet(f"data/intermediate/census_variables/{filename}")
+    logger.info(f"GENERATED file {filename}")
 
-    # assign a file name based on dataset name and variable
-    filename = f'{cfg.census.dataset_name}__{variable}.parquet'
-    #var_df.reset_index(inplace=True)
-    var_df.to_parquet(f'data/intermediate/census_variables/{filename}')
-    print(f"GENERATED file {filename}")
 
 if __name__ == "__main__":
     main()
-    
--- src/merge_variables.py
+++ src/merge_variables.py
@@ -1,23 +1,31 @@
-import pandas as pd
+import logging
+
 import duckdb
 import hydra
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
 
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(cfg):
     # == concat the df's ==
     all_vars_list = []
-    for variable in cfg.census.variables.keys():
-        var_df = pd.read_parquet(f'data/intermediate/census_variables/{cfg.census.dataset_name}__{variable}.parquet')
+    for variable in cfg.variable_codes.keys():
+        var_df = pd.read_parquet(
+            f"data/intermediate/census_variables/{cfg.geo_type}__{cfg.survey}__{variable}.parquet"
+        )
         all_vars_list.append(var_df)
     all_vars_df = pd.concat(all_vars_list, axis=1)
 
     # == save parquet ==
-    filename = f'data/output/census_series/{cfg.census.dataset_name}.parquet'
+    filename = f"data/output/census_series/{cfg.geo_type}__{cfg.survey}.parquet"
     con = duckdb.connect()
     all_vars_df.reset_index(inplace=True)
-    con.register('all_vars', all_vars_df)
+    con.register("all_vars", all_vars_df)
     con.execute(f"COPY all_vars TO '{filename}' (FORMAT PARQUET)")
-    print(f"GENERATED file {filename}")
+    logger.info(f"GENERATED file {filename}")
+
 
 if __name__ == "__main__":
     main()
