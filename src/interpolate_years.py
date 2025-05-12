da93907510edb516ec6c81e6c2df893f249b87c1
James Celi Kitch <jckitch31@gmail.com>
2025-05-12 11:31:27 -0400

    file cleanup + increase abstractability

--- src/interpolate_years.py
+++ src/interpolate_years.py
@@ -13,8 +13,19 @@ def main(cfg):
     min_yr = cfg.interp.min_interp_year
     max_yr = cfg.interp.max_interp_year
 
-    df_00 = pd.read_parquet(f"{cfg.interp.input_dir}/{cfg.interp.survey_dict[min_yr]}_{min_yr}.parquet")
-    df_09 = pd.read_parquet(f"{cfg.interp.input_dir}/{cfg.interp.survey_dict[max_yr]}_{max_yr}.parquet")
+    sp_res = cfg.interp.sp_res
+    var_dict = cfg.variables.valid_years[sp_res]
+
+    # construct survey dict
+    # Start with dec years
+    survey_dict = {yr: "dec" for yr in var_dict["dec"]}
+    # Add acs5 years only if not already in the dict
+    for yr in var_dict["acs5"]:
+        if yr not in survey_dict:
+            survey_dict[yr] = "acs5"
+
+    df_start = pd.read_parquet(f"{cfg.interp.input_dir}/{survey_dict[min_yr]}_{min_yr}.parquet")
+    df_end = pd.read_parquet(f"{cfg.interp.input_dir}/{survey_dict[max_yr]}_{max_yr}.parquet")
 
     year_lst = range(min_yr, max_yr+1)
     year_gap = len(year_lst)
@@ -23,15 +34,15 @@ def main(cfg):
     # read in crosswalk file
     if cfg.interp.intersect_files is not None:
         zcta_xwalk = pd.read_parquet(cfg.interp.intersect_files)
-        # merge with df_00``
-        df_00 = df_00.merge(zcta_xwalk, left_on="zcta", right_on="zcta_00", how="inner")
+        # merge with df_start
+        df_start = df_start.merge(zcta_xwalk, left_on="zcta", right_on="zcta_00", how="inner")
         
         # Keep only necessary columns
-        df_00_2_10 = df_00[var_lst + ["weight", "zcta_10"]].copy()
+        df_start = df_start[var_lst + ["weight", "zcta_10"]].copy()
 
         # Group and compute weighted mean
-        df_00_2_10 = (
-            df_00_2_10.groupby("zcta_10")
+        df_start = (
+            df_start.groupby("zcta_10")
             .apply(lambda g: pd.Series({
                 var: weighted_mean(g[var], g["weight"])
                 for var in var_lst
@@ -39,10 +50,10 @@ def main(cfg):
             .reset_index()
         )
         # merge in 2010 file
-        df_mg = df_00_2_10.merge(df_09, left_on="zcta_10", right_on="zcta", how="inner", suffixes=("_00", "_10"))
+        df_mg = df_start.merge(df_end, left_on="zcta_10", right_on="zcta", how="inner", suffixes=("_start", "_end"))
     else:
         # otherwise match purely on same zctas, no xwalk
-        df_mg = df_00.merge(df_09, on="zcta", how="inner", suffixes=("_00", "_10"))
+        df_mg = df_start.merge(df_end, on="zcta", how="inner", suffixes=("_start", "_end"))
 
     uniq_zcta = df_mg["zcta"].unique()
 
@@ -50,12 +61,12 @@ def main(cfg):
     var_diff = {}
     for var in var_lst:
         # make sure variable in both data frames
-        if f"{var}_00" in df_mg.columns and f"{var}_10" in df_mg.columns:
-            var_diff[var] = df_mg[f"{var}_10"] - df_mg[f"{var}_00"]
+        if f"{var}_start" in df_mg.columns and f"{var}_end" in df_mg.columns:
+            var_diff[var] = df_mg[f"{var}_end"] - df_mg[f"{var}_start"]
     
 
     for curr_step in range(year_gap):
-        df_out = pd.DataFrame({var: df_mg[f"{var}_00"] + var_diff[var]/(year_gap - curr_step) for var in var_diff.keys()})
+        df_out = pd.DataFrame({var: df_mg[f"{var}_start"] + var_diff[var]/(year_gap - curr_step) for var in var_diff.keys()})
         df_out["zcta"] = df_mg["zcta"]
         df_out["year"] = int(year_lst[curr_step])
         df_out.to_parquet(f"{cfg.interp.out_dir}/{cfg.interp.base_name}__{year_lst[curr_step]}.parquet")
@@ -66,8 +77,10 @@ def main(cfg):
 
     # additionally copying and editing filenames for more recent years
     for year in range(cfg.interp.max_interp_year, cfg.interp.max_census_year+1):
-        df = pd.read_parquet(f"{cfg.interp.input_dir}/{cfg.interp.survey_dict[year]}_{year}.parquet")
-        if cfg.interp.trim_2010_yrs:
+        df = pd.read_parquet(f"{cfg.interp.input_dir}/{survey_dict[year]}_{year}.parquet")
+
+        # option to enforce constant zctas across all census files
+        if cfg.interp.trim_zctas:
             df = df[df["zcta"].isin(uniq_zcta)]
         df.to_parquet(f"{cfg.interp.out_dir}/{cfg.interp.base_name}__{year}.parquet")
 
